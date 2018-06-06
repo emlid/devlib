e2c0f4cfc7c6c19bb55b98a0dea34a10cc7b3952
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-06 18:06:19 +0300

    add examples module

---
+++ examples/devlib_cli/main.cpp
@@ -0,0 +1,39 @@
+#include <QCoreApplication>
+#include "devlib.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    auto service = devlib::StorageDeviceService::instance();
+    Q_ASSERT(service.get());
+
+    auto devices = service->getAvailableStorageDevices();
+
+    for (auto const& device : devices) {
+        qInfo() << "device" << '\n'
+                << "+ vid: " << device->vid() << '\n'
+                << "+ pid: " << device->pid() << '\n'
+                << "+ fsPath: " << device->filePath();
+
+        qInfo() << "\n + mntpts:";
+        for (auto const& mntpt : device->mountpoints()) {
+            qInfo() << "  +- name: " << mntpt->fsPath();
+        }
+
+        qInfo() << "\n + partitions(volumes):";
+        for (auto const& part : device->partitions()) {
+            qInfo() << "   +- name: " << part->filePath() << '\n'
+                    << "  +- label: " << part->label();
+            qInfo() << "   +- Mounpoints:";
+
+            for (auto const& mntpt : part->mountpoints()) {
+                qInfo() << "      +- " << mntpt->fsPath();
+            }
+
+            qInfo() << "";
+        }
+    }
+
+    return a.exec();
+}
