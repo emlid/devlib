98df747dd551dfca299e11ce405c603e1691d490
Igor Anokhin <igor.anohin@emlid.com>
2018-06-20 15:52:51 +0300

    examples: devlib_cli: main: add usbPortPath support

--- examples/devlib_cli/main.cpp
+++ examples/devlib_cli/main.cpp
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
         qInfo() << "device" << '\n'
                 << "+ vid: " << device->vid() << '\n'
                 << "+ pid: " << device->pid() << '\n'
-                << "+ fsPath: " << device->filePath();
+                << "+ fsPath: " << device->filePath() << '\n'
+                << "+ usbPortPath: " << device->usbPortPath();
 
         qInfo() << "\n + mntpts:";
         for (auto const& mntpt : device->mountpoints()) {
