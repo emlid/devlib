5b373f81bded9d0029d1cf17ffc7548b2a63915c
Igor Anokhin <igor.anohin@emlid.com>
2018-06-20 15:46:40 +0300

    devlib: native: requestUsbDeviceList tuple
    Add new QString field for device location

--- devlib/native/native.h
+++ devlib/native/native.h
@@ -25,7 +25,7 @@ namespace devlib {
             -> std::vector<std::pair<QString, QString>>;
 
         auto requestUsbDeviceList(void)
-            -> std::vector<std::tuple<int, int, QString>>;
+            -> std::vector<std::tuple<int, int, QString, QString>>;
 
         auto devicePartitions(QString const& deviceName)
             -> std::vector<std::tuple<QString, QString>>;
